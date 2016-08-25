dd944b3dae26e27eea9a7bda925fea4e060d4f20
AimeDeb <aime.debonne@gmal.com>
2016-08-25 09:25:29 +0200

    [COR] panier.html : correction lien + / -

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -1,22 +1,24 @@
 (function(){
-    $(function(){
-        $('.add-product-json-link').on('click', function (event) {
-            event.preventDefault();
-            var $this = $(this);
-            $.ajax({
-                url: $this.attr('href'),
-                type: 'GET'
-            }).done(function(cart){
-                var cartHTML = "<ul>";
-                for(var product of cart.produits){
-                    cartHTML += '<li class="cart-item">' + product.produit.description + '</li>';
-                }
-                cartHTML += "</ul>";
-                $('#cart').html(cartHTML);
-
-            }).fail(function(error){
-                console.error(error);
-            });
+    $('#formAjoutPanier').on('submit', function(event) {
+        event.preventDefault();
+        var $this = $(this);
+        $.ajax({
+            url: $this.attr('action'),
+            type: "POST",
+            data: $this.serialize()
+        }).done(function(resultat){
+            if(resultat == true){
+                document.getElementById("formAjoutPanier-resOK").removeClass("hidden");
+                document.getElementById("formAjoutPanier-resN").addClass("hidden");
+            }
+            else{
+                document.getElementById("formAjoutPanier-resOK").addClass("hidden");
+                document.getElementById("formAjoutPanier-resN").removeClass("hidden");
+            }
+        }).fail(function(error){
+            console.error(error);
+            document.getElementById("formAjoutPanier-resOK").addClass("hidden");
+            document.getElementById("formAjoutPanier-resN").removeClass("hidden");
         });
-    })
+    });
 })();
